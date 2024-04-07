4230cd33c9bef0d2e2342fe5e87d6bd5d134db8e
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-07 20:32:08 +0900

    Add new GET /derivative_expression => derivative_expression#show routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@
 
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
+
+  get "derivative_expression", to: "derivative_expression#show", only: %i[show]
 end
