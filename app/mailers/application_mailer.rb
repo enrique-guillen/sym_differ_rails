3fea92f12f2381436f92d6da0ee6ee8d95ef5c32
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-07 18:23:15 +0900

    Document ApplicationMailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Inherits from ActionMailer::Base and acts as an abstract class for Rails mailer classes.
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
   layout "mailer"
