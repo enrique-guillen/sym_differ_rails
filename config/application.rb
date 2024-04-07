c5e3276c417be084a0acdeb03e89a1b814d0c44e
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-07 20:31:25 +0900

    change require rails/all to multiple require-specific-framework calls in config/application

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,14 @@
 
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
 
