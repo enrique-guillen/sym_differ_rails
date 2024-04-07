7537d367cbe58efb240ab56fecd61f17e04c407f
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-07 18:22:34 +0900

    Document ApplicationJob

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Inherits from ActiveJob::Base and acts as an abstract class for Rails jobs.
 class ApplicationJob < ActiveJob::Base
   retry_on ActiveRecord::Deadlocked
 
