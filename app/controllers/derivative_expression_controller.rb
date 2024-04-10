01a9e5cde1dd97a1cb407e9173f4a2ccf0360efd
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-10 15:49:24 +0900

    DerivativeExpressionController#show sets the access control allow origin value to localhost

--- app/controllers/derivative_expression_controller.rb
+++ app/controllers/derivative_expression_controller.rb
@@ -5,12 +5,21 @@ require "sym_differ/get_derivative_of_expression_director"
 # Allows computing the derivative of a given expression.
 class DerivativeExpressionController < ApplicationController
   def show
+    calculate_derivate
+    set_access_control_allow_origin_to_localhost
+    render(formats: %i[json])
+  end
+
+  private
+
+  def calculate_derivate
     @operation_response =
       SymDiffer::GetDerivativeOfExpressionDirector
       .new
       .calculate_derivative(params.require(:expression), params.require(:variable))
+  end
 
+  def set_access_control_allow_origin_to_localhost
     response.set_header("Access-Control-Allow-Origin", "http://localhost:5173")
-    render(formats: %i[json])
   end
 end
--- spec/requests/derivative_expression_spec.rb
+++ spec/requests/derivative_expression_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe "DerivativeExpressions" do
     it "renders the expected value" do
       get("/derivative_expression", params: { expression: "x + x", variable: "x" })
       expect(response.parsed_body).to eq("derivative_expression" => "2")
+      expect(response.headers).to include("Access-Control-Allow-Origin" => "http://localhost:5173")
     end
   end
 end
