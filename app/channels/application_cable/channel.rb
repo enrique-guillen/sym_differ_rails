23da192dab0e28d8bb9df8cfddb0c72c3c8f627b
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-03 16:22:54 +0900

    Document application_cable modules

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationCable
+  # Encapsulates logic between channels.
   class Channel < ActionCable::Channel::Base
   end
 end
--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationCable
+  # Authorize the incomming connection and proceeed to establish it if the user can be identified.
   class Connection < ActionCable::Connection::Base
   end
 end
