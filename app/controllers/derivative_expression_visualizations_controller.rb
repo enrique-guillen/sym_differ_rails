f65148394436661be10e52137a52534bbe95121c
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-13 21:48:23 +0900

    Use new AccessControlConcern and SymDifferExceptionHandlingConcern in DerivativeExpressionVisualizationsController

--- app/controllers/derivative_expression_visualizations_controller.rb
+++ app/controllers/derivative_expression_visualizations_controller.rb
@@ -5,37 +5,22 @@ require "sym_differ/error"
 
 # Allows visualization the derivative of a given expression.
 class DerivativeExpressionVisualizationsController < ApplicationController
+  include SymDifferExceptionHandlingConcern
+  include AccessControlConcern
+
   def show
-    set_access_control_allow_origin_to_localhost
-    visualize
+    response = visualize
 
-    send_data @operation_response.image, type: "image/svg+xml"
+    send_data response.image, type: "image/svg+xml"
   rescue SymDiffer::Error => e
-    @sym_differ_exception = e.cause || e
-    view_name = view_name_for_exception_cause(e)
-
-    render "global_exceptions/#{view_name}", status: :unprocessable_entity, formats: %i[json]
+    handle_sym_differ_error(e)
   end
 
   private
 
-  def set_access_control_allow_origin_to_localhost
-    response.set_header("Access-Control-Allow-Origin", "http://localhost:5173")
-  end
-
   def visualize
     @operation_response =
       visualize_interactor.visualize(params.fetch(:expression), params.fetch(:variable))
-  end
-
-  def view_name_for_exception_cause(exception)
-    return underscore_class_name(exception.cause) if exception.cause
-
-    underscore_class_name(exception)
-  end
-
-  def underscore_class_name(object)
-    object.class.name.underscore.split("/").last
   end
 
   def visualize_interactor
